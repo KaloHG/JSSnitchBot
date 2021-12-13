ad0b98f09b845ad460e8f55085e491a3fd36ea6b
saviorseven11@gmail.com <saviorseven11@gmail.com>
2021-12-12 20:27:00 -0800

    Skynet Update

--- index.js
+++ index.js
@@ -7,28 +7,27 @@ const mineflayer = require('mineflayer');
 const ip = "mc.civserver.xyz"; //Server IP
 const snitchChannel = ""; //Discord Channel ID for the Snitch Channel (http://img.kayla.moe/Discord_2z7NFHdNJw.png)
 const globalChannel = ""; //Same as above
+const skynetChannel = ""; //Join and leave channel
 const token = "token"; //Make an application on Discord and copy the bot token (https://canary.discord.com/developers/applications)
 
-//Do not touch these.
+//Do nto touch
 const snitchReg = /^(Login|Enter|Logout|Leave)  ([A-Za-z0-9_]{2,16})  (\S*)  \[(-?[0-9]+) (-?[0-9]+) (-?[0-9]+)\]  \[([0-9]+)m ((?:North|South|East|West)(?: North| South| East| West)?)\]/
 
 const globalReg = /\[([\S]+)\] ([\S:]+) (.+)$/
 
-var regExample = "Enter  PlayerName!  SnitchName1234_!  [-213 2 643]  [100m North]";
+var regExample = "Enter  Xdddd3314  Ur_mum_gay  [-213 2 643]  [100m North]";
 
 var regResult = snitchReg.exec(regExample);
 
 var options = {
 	host: ip, // minecraft server ip
-  username: '', // minecraft username/email
+  username: '', // minecraft username
   password: '', // minecraft password, comment out if you want to log into online-mode=false servers
   // port: 25565,                // only set if you need a port that isn't 25565
-  //Set below specifically for the latest version on the server. If you set it automatically it may bug out with ViaVersion.
    version: "1.16.5"             // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
   // auth: 'mojang'              // only set if you need microsoft auth, then set this to 'microsoft'
 }
 
-//Debug check to make sure the regex is working.
 console.log(regResult);
 console.log('Type: ' + regResult[1] + '\nPlayer: ' + regResult[2] + '\nName: ' + regResult[3] + '\nX: ' + regResult[4] + '\nY: ' + regResult[5] + '\nZ: ' + regResult[6] + '\nDistance: ' + regResult[7] + '\nDirection: ' + regResult[8]);
 
@@ -100,6 +99,24 @@ function bindEvents(bot) {
 		}
 	}
 		console.log(jsonMsg + "\nPosition: " + position);
+	});
+
+	bot.on('playerJoined', (player) => {
+		const channel = client.channels.cache.get(skynetChannel);
+		channel.send('**' + player.displayName +'** JOINED.').catch(console.error,() => {
+			console.log("Cannot send messages to SKYNET channel! Fix please");
+			return;
+		});
+		channel.setTopic('Players logged in: ' + bot.players.size);
+	});
+
+	bot.on('playerLeft', (player) => {
+		const channel = client.channels.cache.get(skynetChannel);
+		channel.send('**' + player.displayName +'** LEFT.').catch(console.error,() => {
+			console.log("Cannot send messages to SKYNET channel! Fix please");
+			return;
+		});
+		channel.setTopic('Players logged in: ' + bot.players.size);
 	});
 
 	bot.on('end', function() {
